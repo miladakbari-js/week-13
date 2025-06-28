7d61b787dd144ebbb4bce5d70b2b484ac4b66bab
Milad Akbari <milad.akbari.codevelop@gmail.com>
2025-06-28 22:16:19 +0330

    create data center for information

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -3,7 +3,6 @@ function Modal() {
   return (
     <div className="container-modal">
         <div className="modal-head">
-            {/* <button>X</button> */}
             <div className="round" style={{backgroundColor:"#eb3434"}}></div>
             <div className="round" style={{backgroundColor:"#dddddf"}}></div>
             <div className="round" style={{backgroundColor:"#66af66"}}></div>
---
+++ src/constants/data.js
@@ -0,0 +1,6 @@
+const topics = [
+  { name: "Html-css", description: "", image: "" },
+  { name: "javascript", description: "", image: "" },
+  { name: "react", description: "", image: "" },
+  { name: "next", description: "", image: "" },
+];
